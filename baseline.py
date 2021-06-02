bde7068f0a60037d35a64e4d036615b54f911d04
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2021-06-02 14:03:42 +0300

    Baseline with score: 12.317291

---
+++ baseline.py
@@ -0,0 +1,82 @@
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import StratifiedKFold
+
+import settings
+
+
+def get_data():
+    train_df = pd.read_csv(
+        settings.TRAIN,
+        parse_dates=['created_at']
+    )
+    test_df = pd.read_csv(
+        settings.TEST,
+        parse_dates=['created_at']
+    )
+    sub_df = pd.read_csv(
+        settings.BASELINE_SUBMISIION
+    )
+    sub_df['ref_pm2_5'] = 0
+    return train_df, test_df, sub_df
+
+
+def create_features(df):
+    df['year'] = df['created_at'].dt.year
+    df['month'] = df['created_at'].dt.month
+    df['day'] = df['created_at'].dt.day
+    df['hour'] = df['created_at'].dt.hour
+    df['minute'] = df['created_at'].dt.minute
+    df['weekday'] = df['created_at'].dt.weekday
+    return df
+
+
+def create_submission(train_df, test_df, sub_df):
+    folds = StratifiedKFold(
+        n_splits=settings.N_FOLDS
+    )
+    clf = lgb.LGBMRegressor(**settings.LGBM_PARAMS)
+    scores = []
+
+    for trn_idx, val_idx in folds.split(
+        train_df[settings.FEATURES],
+        train_df['site']
+    ):
+        clf.fit(
+            X=train_df.loc[trn_idx, settings.FEATURES],
+            y=train_df.loc[trn_idx, settings.TARGET],
+            eval_set=[(
+                train_df.loc[val_idx, settings.FEATURES],
+                train_df.loc[val_idx, settings.TARGET]
+            )],
+            eval_metric='rmse',
+            verbose=1000
+        )
+        pred = clf.predict(train_df.loc[val_idx, settings.FEATURES])
+        score = np.sqrt(mean_squared_error(
+            train_df.loc[val_idx, settings.TARGET],
+            pred
+        ))
+        scores.append(score)
+        sub_df['ref_pm2_5'] += clf.predict(
+            test_df[settings.FEATURES]
+        ) / settings.N_FOLDS
+    mean_score = np.mean(scores)
+    print(mean_score)
+    sub_df.to_csv(
+        settings.MAIN_PATH / 'subs' / f'lgb-{mean_score:2.8}.csv',
+        index=False
+    )
+
+
+def main():
+    train_df, test_df, sub_df = get_data()
+    train_df = create_features(train_df)
+    test_df = create_features(test_df)
+    create_submission(train_df, test_df, sub_df)
+
+
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+MAIN_PATH = Path('./')
+TRAIN = MAIN_PATH / 'data' / 'train.csv'
+TEST = MAIN_PATH / 'data' / 'test.csv'
+BASELINE_SUBMISIION = MAIN_PATH / 'data' / 'submission.csv'
+
+N_FOLDS = 4
+RANDOM_STATE = 1
+
+TARGET = 'ref_pm2_5'
+FEATURES = [
+    'year',
+    'month',
+    'day',
+    'hour',
+    # 'minute',
+    'weekday',
+    'pm2_5',
+    'pm10',
+    's2_pm2_5',
+    's2_pm10',
+    'humidity',
+    'temp',
+    # 'lat',
+    # 'long',
+    # 'altitude',
+    # 'greenness',
+    # 'landform_90m',
+    # 'landform_270m',
+    'population',
+    'dist_major_road'
+]
+
+LGBM_PARAMS = {
+    'random_state': RANDOM_STATE,
+    'num_leaves': 15,
+    'learning_rate': .01,
+    'metric': 'rmse',
+    'num_iteration': 7000
+}
